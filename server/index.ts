70bc5fc54d1206e28090887ab81220d494a80361
Omar.Abdelaziz <46730385+OmarAbdelazizMahmed@users.noreply.github.com>
2022-12-21 01:49:51 +0200

    Feat: index

---
+++ server/index.ts
@@ -0,0 +1,34 @@
+import express, { RequestHandler } from 'express';
+
+const app = express();
+
+// parse application/json
+app.use(express.json());
+
+
+const posts: any[] = [];
+
+const requestLoggerMiddleware: RequestHandler = (req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+};
+
+app.use(requestLoggerMiddleware);
+
+app.get('/posts', (req, res) => {
+    res.send({ posts });
+});
+
+
+
+app.post('/posts', (req, res) => {
+    const post = req.body;  
+    posts.push(post);
+    res.send('post added');
+
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+    }
+);
